3fb9c3c082171dc2879db699987da55e4204ab07
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-08-14 17:12:00 +0300

    fix: build error

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,5 +2,4 @@ export { CardList } from './CardList/CardList';
 export { Flyout } from './Flyout/Flyout';
 export { Footer } from './Footer/Footer';
 export { Header } from './Header/Header';
-export { ImageDetails } from './ImageDetails/ImageDetails';
 export { SearchBar } from './SearchBar/SearchBar';
